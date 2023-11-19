1f32cdaab85a639dd70e44c57fdd9259a1b940dd
Gokul A <gokulprathin8@gmail.com>
2023-11-18 20:19:00 -0500

    changes to capture snow cover

--- main.py
+++ main.py
@@ -14,41 +14,50 @@ tile_list = ["h08v04", "h08v05", "h09v04", "h09v05", "h10v04", "h10v05", "h11v04
              "h13v04", "h13v05", "h15v04", "h16v03", "h16v04", ]
 input_folder = os.getcwd() + "/temp/"
 output_folder = os.getcwd() + "/output_folder/"
+modis_day_wise = os.getcwd() + "/final_output/"
 os.makedirs(output_folder, exist_ok=True)
+os.makedirs(modis_day_wise, exist_ok=True)
+
 
 def convert_hdf_to_geotiff(hdf_file, output_folder):
-  # Open the HDF file
   hdf_ds = gdal.Open(hdf_file, gdal.GA_ReadOnly)
 
-  # Loop through each subdataset in the HDF file
+  # Specific subdataset name you're interested in
+  target_subdataset_name = "MOD_Grid_Snow_500m:NDSI_Snow_Cover"
+
   for subdataset in hdf_ds.GetSubDatasets():
-    # Extract the name of the subdataset
-    name = subdataset[0].split(':')[-1]
+    # Check if the subdataset is the one we want to convert
+    if target_subdataset_name in subdataset[0]:
+      ds = gdal.Open(subdataset[0], gdal.GA_ReadOnly)
 
-    # Open the subdataset
-    ds = gdal.Open(subdataset[0], gdal.GA_ReadOnly)
+      # Create a name for the output file based on the HDF file name and subdataset
+      output_file_name = os.path.splitext(os.path.basename(hdf_file))[0] + ".tif"
+      output_path = os.path.join(output_folder, output_file_name)
 
-    # Define the output path
-    output_path = os.path.join(output_folder, f"{name}.tif")
+      # Convert to GeoTIFF
+      gdal.Translate(output_path, ds)
+      ds = None
+      break  # Exit the loop after converting the target subdataset
 
-    # Convert to GeoTIFF
-    gdal.Translate(output_path, ds)
-
-    # Close the subdataset
-    ds = None
-
-  # Close the HDF dataset
   hdf_ds = None
 
 
 def convert_all_hdf_in_folder(folder_path, output_folder):
-  # List all files in the given folder
+  file_lst = list()
   for file in os.listdir(folder_path):
-    # Check if the file is an HDF file
+    file_lst.append(file)
     if file.lower().endswith(".hdf"):
       hdf_file = os.path.join(folder_path, file)
       convert_hdf_to_geotiff(hdf_file, output_folder)
       print(f"Converted {file} to GeoTIFF")
+  return file_lst
+
+
+def merge_tifs(folder_path, output_file):
+  tif_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.tif')]
+  # gdal_command = ['gdal_merge.py', '-o', output_file, '-of', 'GTiff', '-r', 'cubic'] + tif_files
+  gdal_command = gdal_command = ['gdalwarp', '-r', 'cubic'] + tif_files + [output_file]
+  subprocess.run(gdal_command)
 
 
 def list_files(directory):
@@ -100,19 +109,33 @@ def download_all(date, urls):
     thread.join()
 
 
+def delete_files_in_folder(folder_path):
+  if not os.path.exists(folder_path):
+    print("Folder does not exist.")
+    return
+
+  for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    try:
+      if os.path.isfile(file_path) or os.path.islink(file_path):
+        os.unlink(file_path)
+      else:
+        print(f"Skipping {filename}, as it is not a file.")
+    except Exception as e:
+      print(f"Failed to delete {file_path}. Reason: {e}")
+
+
 def download_tiles_and_merge():
   date_list = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
   for i in date_list:
     current_date = i.strftime("%Y-%m-%d")
     earthaccess.login(strategy="netrc")
-    results = earthaccess.search_data(short_name="MOD10A1", cloud_hosted=True,
-      bounding_box=(-124.77, 24.52, -66.95, 49.38), temporal=(current_date, current_date)
-      # start date and end date are same to consider 1 day
-    )
-    current_working_dir = os.getcwd()
+    results = earthaccess.search_data(short_name="MOD10A1", cloud_hosted=True, bounding_box=(-124.77, 24.52, -66.95, 49.38),
+                            temporal=(current_date, current_date))
     earthaccess.download(results, input_folder)
     convert_all_hdf_in_folder(input_folder, output_folder)
-    exit()
-
+    merge_tifs(folder_path=output_folder, output_file=f'{modis_day_wise}/{current_date}__snow_cover.tif')
+    delete_files_in_folder(input_folder)  # cleanup
+    delete_files_in_folder(output_folder)  # cleanup
 
 download_tiles_and_merge()
